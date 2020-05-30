d26efea88d7bee6f20e36dec931463d637055ac9
elbaro <elbaro@users.noreply.github.com>
2020-05-30 19:49:39 +0900

    use cmake

---
+++ recipe/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -ex
+
+mkdir -p $PREFIX/include
+cp doctest.h $PREFIX/include/doctest.h  # [unix]
+
+
+mkdir -p out/release
+cd out/release
+cmake \
+  -GNinja \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_INSTALL_PREFIX=$PREFIX \
+  -DDOCTEST_WITH_TESTS=OFF \
+  -DDOCTEST_WITH_MAIN_IN_STATIC_LIB=OFF \
+  ../..
+
+ninja install
