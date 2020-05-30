2b437b672077796fe71594a0b5614b86d8c16444
elbaro <elbaro@users.noreply.github.com>
2020-05-30 20:36:40 +0900

    typo

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,7 +7,7 @@ cmake \
   -GNinja \
   -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_INSTALL_PREFIX=$PREFIX \
-  -DCMAKE_INSTALL_LIBDIR=lib \  # without this, cmake installs to lib64
+  -DCMAKE_INSTALL_LIBDIR=lib \
   -DDOCTEST_WITH_TESTS=OFF \
   -DDOCTEST_WITH_MAIN_IN_STATIC_LIB=OFF \
   ../..
